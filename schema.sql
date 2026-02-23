15aa1e326ce6b2c8cc42d4f46a0656715a191911
tm2533 <tm2533@bath.ac.uk>
2026-02-23 19:27:33 +0000

    Define database schema in schema.sql

---
+++ schema.sql
@@ -0,0 +1,73 @@
+PRAGMA foreign_keys = ON;
+
+-- ========================================================================
+-- Flight table
+-- ========================================================================
+CREATE TABLE IF NOT EXISTS Flight (
+    FlightId                    INTEGER PRIMARY KEY AUTOINCREMENT,
+    FlightNumber                TEXT NOT NULL UNIQUE,
+    AircraftId                  INTEGER NOT NULL, 
+    DepartureAirportId          INTEGER NOT NULL, 
+    DestinationAirportId        INTEGER NOT NULL, 
+    DepartureTime               TEXT NOT NULL, 
+    DestinationArrivalTime      TEXT NOT NULL, 
+    FlightStatus                TEXT NOT NULL CHECK (FlightStatus IN ('SCHEDULED', 'DELAYED', 'CANCELLED', 'DEPARTED', 'ARRIVED')
+    ),
+
+    FOREIGN KEY (AircraftId) REFERENCES Aircraft(AifcraftId) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (DepartureAirportId) REFERENCES Destination(DestinationId) ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY (DestinationAiportId) REFERENCES Destination(DestinationId) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+-- ========================================================================
+-- Pilot table
+-- ========================================================================
+CREATE TABLE IF NOT EXISTS Pilot (
+    PilotId                     INTEGER PRIMARY KEY AUTOINCREMENT, 
+    LicenseNumber               TEXT NOT NULL UNIQUE, 
+    FirstName                   TEXT NOT NULL,
+    MiddleName                  TEXT,
+    LastName                    TEXT NOT NULL,
+    MobilePhone                 TEXT,
+    Email                       TEXT,
+    EmploymentStartDate         TEXT NOT NULL,
+    EmploymentEndDate           TEXT NOT NULL,
+    IsActive                    INTEGER NOT NULL DEFAULT 1 CHECK (IsActive IN (0, 1))
+);
+
+-- ========================================================================
+-- Flight_Pilot table
+-- ========================================================================
+CREATE TABLE IF NOT EXISTS Flight_Pilot (
+    FlightId        INTEGER NOT NULL,
+    PilotId         INTEGER NOT NULL,
+
+    PRIMARY KEY (FlightId, PilotId),
+
+    FOREIGN KEY (FlightId) REFERENCES Flight(FlightId) ON DELETE CASCADE,
+    FOREIGN KEY (PilotId) REFERENCES Pilot(PilotId) ON DELETE RESTRICT
+);
+
+-- ========================================================================
+-- Aircraft table
+-- ========================================================================
+CREATE TABLE IF NOT EXISTS Aircraft (
+    AircraftId          INTEGER PRIMARY KEY AUTOINCREMENT,
+    Model               TEXT NOT NULL,
+    PassengerCapacity   INTEGER NOT NULL,
+    BuiltDate           TEXT NOT NULL,
+    LastCheckDate       TEXT NOT NULL
+);
+
+
+-- ========================================================================
+-- Destination table
+-- ========================================================================
+CREATE TABLE IF NOT EXISTS Destination (
+    DestinationId       INTEGER PRIMARY KEY AUTOINCREMENT,
+    AirportCode         TEXT NOT NULL UNIQUE,
+    AirportName         TEXT NOT NULL,
+    City                TEXT NOT NULL,
+    Country             TEXT NOT NULL,
+    Terminal
+);
