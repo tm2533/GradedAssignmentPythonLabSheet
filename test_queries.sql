0005a163fc6458ba4941ad3a96457d7d696b4d72
tm2533 <tm2533@bath.ac.uk>
2026-02-24 01:45:57 +0000

    Add a test query to verify a new flight was added

--- test_queries.sql
+++ test_queries.sql
@@ -10,8 +10,12 @@ SQL queries below are used to verify that it is possible to query the created da
 -- ========================================================================
 . schema
 
+-- Verify that the tables were created successfully.
 SELECT * FROM Flight LIMIT 5;
 SELECT * FROM Pilot LIMIT 5;
 SELECT * FROM Flight_Pilot LIMIT 5;
 SELECT * FROM Aircraft LIMIT 5;
 SELECT * FROM Destination LIMIT 5;
+
+-- If the user added a new flight, use the query below to verify that the new flight was added successfully.
+SELECT * FROM Flight WHERE FlightId > 15 ORDER BY FlightId;
